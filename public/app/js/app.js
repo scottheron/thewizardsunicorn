8db541a9387ed545a4d8932539155d063b3414b2
Scott <scott.heron@outlook.com>
2016-04-04 09:47:04 -0700

    integrated speech js files and created routes with some styling

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -8,12 +8,16 @@ app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
         $stateProvider
         .state('home', {
             url: '/',
-            templateUrl: 'app/views/voicetotext.html',
+            templateUrl: 'app/views/landing.html'
             // controller: ''
+        })
+        .state('voicetotext', {
+            url: '/voicetotext',
+            templateUrl: 'app/views/voicetotext.html'
         })
         .state('adventure', {
             url: '/adventure',
-            templateUrl: 'app/views/adventure.html',
+            templateUrl: 'app/views/adventure.html'
             //controller: 'adventure'
         })
         .state('404', {
