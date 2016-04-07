18fb302010dcb0a64d3577cc5e6e0f1af140dfb9
Scott <scott.heron@outlook.com>
2016-04-06 20:44:29 -0700

    added game services

--- public/app/js/services.js
+++ public/app/js/services.js
@@ -1,4 +1,4 @@
-var svc = angular.module('AdventureSrvc', ['ngResource']);
+var svc = angular.module('AdventureService', ['ngResource']);
 
 svc.factory('Adventure', ['$resource', function($resource) {
   return $resource('/api/adventure/:id');
@@ -32,4 +32,109 @@ svc.factory('AuthInterceptor', ['Auth', function(Auth) {
       return config;
     }
   }
+}]);
+
+svc.service("ParsingService", [function (command){
+    this.parse = function(command){
+        var properItems = {
+            "go": ["lair", "giza", "harbor", "alexandria", "vault", "atlantis"],
+            "pick": ["map", "cauldron", "Haggis", "money", "cat", "schwarma", "light", "History", "Squash", "dust", "cake"]
+        };
+        par = command.split(" ");
+        comm = par[0].trim();
+        // location = par[par.length-1].trim();
+        item = par[par.length-1].trim();
+        console.log(comm);
+        console.log(item);
+
+        if (comm in properItems && properItems[comm].indexOf(item) == -1){
+            comm = "mcGarble";
+        }
+        
+        return {
+            comm,
+            // location,
+            item
+            
+        };
+    }
+}]);
+
+svc.service("GetWizard", ["$http",function ($http){
+   this.wizardDB = function(callback){
+        $http({
+            method: 'GET',
+            url: '/api/wizards'
+        }).then(function successCallback(response) {
+            console.log(response);
+           callback(response);
+        }, function errorCallback(response) {
+            return response;
+        });
+    }
+}]);
+
+svc.service("UpdateWizard", ["$http",function ($http){
+    this.wizardDB = function(theInventory, theLocation, locationHist, finState, theId, callback){
+        $http({
+        method: 'PUT',
+        url: '/api/wizards',
+        data: {
+            id: theId,
+            inventory: theInventory,
+            currentLocation: theLocation,
+            locationHistory: locationHist,
+            fin: finState
+        }
+        }).then(function successCallback(response) {
+            callback(response);
+        }, function errorCallback(response) {
+            
+        });
+    }
+}]);
+
+// svc.service("CreateWizard", [function (theInventory, theLocation, locationHist, finState){
+//     $http({
+//     method: 'POST',
+//     url: '/api/wizard',
+//     data: {
+//         inventory: theInventory,
+//         currentLocation: theLocation,
+//         locationHistory: locationHist,
+//         fin: finState
+//     }
+//     }).then(function successCallback(response) {
+//         this.wizardDB = response;
+//     }, function errorCallback(response) {
+        
+//     });
+// }]);
+
+// svc.service("CreateLocations", [function (aName, someObjects){
+//     $http({
+//     method: 'POST',
+//     url: '/api/location',
+//     data: {
+//         name: aName,
+//         objects: someObjects
+//     }
+//     }).then(function successCallback(response) {
+//         this.locationDB = response;
+//     }, function errorCallback(response) {
+        
+//     });
+// }]);
+
+svc.service("GetLocation", ["$http", function ($http){
+    this.locationDB = function(callback) {
+        $http({
+        method: 'GET',
+        url: '/api/locations'
+        }).then(function successCallback(response) {
+            callback(response);
+        }, function errorCallback(response) {
+            
+        });
+    }
 }]);
